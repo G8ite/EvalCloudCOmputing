ba88f37a130ac00615f9e7d130b094b5d8b329c0
Amandine Pimentel de Andrade <amandine@spirkop.com>
2024-05-21 17:15:45 +0200

    test

--- image_bank/image_bank/settings.py
+++ image_bank/image_bank/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = os.environ.get("BLOB_SECRET_URL")
+STATIC_URL = "https://pimenteldeandradestock.blob.core.windows.net/imageblob/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 ALLOWED_HOSTS = [
     os.environ.get("URL_SERVER_NAME"),
