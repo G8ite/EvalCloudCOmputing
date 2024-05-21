80def685b8b4d5145ce97a60ac32c956bed32666
G8ite <amandine.pda@gmail.com>
2024-05-21 22:10:43 +0200

    Ajoute ReadMe + polish

--- image_bank/images/forms.py
+++ image_bank/images/forms.py
@@ -3,11 +3,6 @@ from .models import Image, Tag
 
 
 class ImageForm(forms.ModelForm):
-    # tags = forms.ModelMultipleChoiceField(
-    #     queryset=Tag.objects.all(),
-    #     widget=forms.CheckboxSelectMultiple
-    # )
-
     class Meta:
         model = Image
         fields = ['image']
